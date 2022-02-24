d1204ac56b56dc4913c39f1079fb0d1138c46c16
jaelee001 <jaehee.lee@kellogg.northwestern.edu>
2022-02-24 03:26:59 +0000

    week_8

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+    def new
+    end
+
+    def create
+        @user = User.where({email: params["email"]})[0]
+        if @user
+                if BCrypt::Password.new(@user.password) == params["password"]
+                    session["user_id"] = @user.id
+                    flash[:notice] = "You logged in!"
+                    redirect_to "/companies"
+                else
+                    flash[:notice] = "nope!"
+                    redirect_to "/sessions/new"
+                end
+        else
+            flash[:notice] = "nope!"
+            redirect_to "/sessions/new"
+        end
+    end
+
+    def destroy
+        session["user_id"] = nil
+        flash[:notice] = "Goodbye."
+        redirect_to "/sessions/new"
+    end
+
+end
+
+
